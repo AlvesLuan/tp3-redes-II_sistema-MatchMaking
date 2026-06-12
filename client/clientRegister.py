7f0de0296c4d145b91248fd307e4892b89163644
Luan K. Santos <luankauan10@gmail.com>
2026-06-12 13:21:58 -0300

    Decode received data before printing
    Decode the received data before printing it to avoid formatting issues.

--- client/clientRegister.py
+++ client/clientRegister.py
@@ -4,13 +4,17 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+'''Recebo todas as informações, junto na string allData e mando pro server, pra lá ele mesmo fazer a separação do que cada coisa é em sua categoria respectiva.'''
 playerName     =   input   ('digite seu id: ')
 game           =   input   ('informe o jogo desejado: ')
 playerLevel    =   input   ('informe seu level: ')
 
 allData = (f"{playerName};{game};{playerLevel}")
+
 clientSocket.sendto(allData.encode(),(serverName, serverPort))
 allData, serverAddress = clientSocket.recvfrom(2048)
 
-print(allData)
+#decode pro print n ficar estranho
+print(allData.decode())
+
 clientSocket.close()
